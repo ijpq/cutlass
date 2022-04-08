db7d5d60d1a24ac62b6d17181c1110e45eb7bd11
任杰(实习) <renjie@megvii.com>
2022-04-08 11:00:19 +0000

    feat(cutlass/all): merge official CUTLASS 2.8

--- ci/run_cutlass_test.sh
+++ ci/run_cutlass_test.sh
@@ -2,10 +2,19 @@
 
 cd $(dirname $0)
 
+exclude_name1="complex"
+exclude_name2="Complex"
+
 cutlass_tests=$(find ./build_cmake/test/unit -name "cutlass_test_unit*" | \
-    grep -v "dir" | grep -v "convolution")
+  grep -v "dir" | grep -v "convolution")
 for cutlass_test in $cutlass_tests
 do
+  if [[ "$cutlass_test" == *"$exclude_name1"* || "$cutlass_test" == *"$exclude_name2"* ]]; then
+   echo "exclude test $cutlass_test"
+   continue
+  else
     echo "run $cutlass_test"
     $cutlass_test
+  fi
+
 done
